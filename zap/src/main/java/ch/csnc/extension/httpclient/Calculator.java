5b3514dd18abcf2e20e90b2d3f134f18cdf45bd5
dipshekhor <datta2107047@stud.kuet.ac.bd>
2025-08-21 09:41:05 +0600

    feature/calcultor updated

---
+++ zap/src/main/java/ch/csnc/extension/httpclient/Calculator.java
@@ -0,0 +1,22 @@
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(int a, int b) {
+        if (b != 0) {
+            return (double) a / b;
+        } else {
+            throw new IllegalArgumentException("Division by zero is not allowed.");
+        }
+    }
+}
