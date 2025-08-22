7614a7536a177938a57468feeb876fd31ea3895b
dipshekhor <datta2107047@stud.kuet.ac.bd>
2025-08-22 13:32:29 +0600

    arekta jinish add korlam

---
+++ zap/src/main/java/ch/csnc/extension/httpclient/Student.java
@@ -0,0 +1,19 @@
+public class Student {
+    private String name;
+    private int age;
+
+    // Constructor
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Getter
+    public String getName() { return name; }
+    public int getAge() { return age; }
+
+    // Setter
+    public void setName(String name) { this.name = name; }
+    public void setAge(int age) { this.age = age; }
+} 
+
